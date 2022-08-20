bc5cb9831d48d32bf170287ed5283337d12a1c00
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2022-08-20 21:43:18 +1000

    Refactor remove unused code

--- wizard.py
+++ wizard.py
@@ -38,65 +38,32 @@ class Wizard(QWizard):
     def _on_device_missing(self) -> None:
         current_page_id = self.currentId()
         if current_page_id > self._device_page_id and current_page_id < self._summary_page_id:
-                #print("Device must be inserted!")
-                #self.setPage(self._device_page_id, self._device_page)  # could memoize this
-                #self.initializePage(current_page_id)
+            lastId = self.currentId()
+            while self.currentId() != self._device_page_id and self.currentId != lastId:
                 lastId = self.currentId()
-                while self.currentId() != self._device_page_id and self.currentId != lastId:
-                    lastId = self.currentId()
-                    self.back()
+                self.back()
 
     def _on_device_locked(self) -> None:
         current_page_id = self.currentId()
         if current_page_id > self._export_page_id and current_page_id < self._summary_page_id:
-            #print("Device must be unlocked!")
-            #self.setPage(self._export_page_id, self._export_page)  # could memoize this
-            #self.initializePage(current_page_id)
             lastId = self.currentId()
             while self.currentId() != self._export_page_id and self.currentId != lastId:
                 lastId = self.currentId()
                 self.back()
-
-    def _on_device_unlocked(self) -> None:
-        #print("_on_device_unlocked")
-        current_page_id = self.currentId()
-        if current_page_id != self._export_page_id:
-            #print("No need to prompt for a passphrase")
-            #self.removePage(self._export_page_id)
-            #self.initializePage(current_page_id)
-            pass
-
-    def _on_device_inserted(self) -> None:
-        current_page_id = self.currentId()
-        if current_page_id != self._device_page_id:
-            #print("No need to prompt for a device to be inserted")
-            #self.removePage(self._device_page_id)
-            #self.initializePage(current_page_id)
-            pass
-        
 
     @pyqtSlot(str)
     def _on_device_state_changed(self, state: Device.State) -> None:
         device_state = self._device.state
         if device_state == Device.MissingState or device_state == Device.RemovedState:
             self._on_device_missing()
-        elif device_state == Device.UnlockedState:
-            self._on_device_inserted()
-            self._on_device_unlocked()
-        elif device_state == Device.UnknownState:
+        elif device_state == Device.UnlockedState or device_state == Device.UnknownState:
             pass
         else:
-            self._on_device_inserted()
             self._on_device_locked()
 
     @pyqtSlot(int)
     def _on_page_changed(self, id: int) -> None:
-        #if id == self._device_page_id:
-        #    self._display_device_state()
         self._on_device_state_changed(self._device.state)
-
-    #def _display_device_state(self) -> None:
-    #    print(f"The device state is currently: {self._device.state}")
 
     def _create_disclaimer_page(self) -> QWizardPage:
         page = QWizardPage()
